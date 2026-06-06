7e839d3ccb82dcfc8ea1051ee7cf9df10613e83e
RyFer <nikulin.rol135720@gmail.com>
2026-06-07 00:01:19 +0300

    Added buisness logic(3)

--- back-end/handlers/films_handler.go
+++ back-end/handlers/films_handler.go
@@ -10,6 +10,7 @@ func GetFilms(w http.ResponseWriter, r *http.Request) {
 	films, err := service.GetFilms()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(films)
